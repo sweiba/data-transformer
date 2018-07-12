84e495c3a604a8ecabf5bd95c01cd539934228cd
Anton Sukharev <sweiba@gmail.com>
2018-07-12 08:55:02 +0500

    Add optional configs and exceptions

--- app/Config.php
+++ app/Config.php
@@ -15,43 +15,54 @@ class Config
 
     public function __construct()
     {
-        $this->config = require $_SERVER["DOCUMENT_ROOT"]."/config/app.php";
+        $this->config = require $_SERVER["DOCUMENT_ROOT"] . "/config/app.php";
     }
 
     public function getInputHandler()
     {
-        switch ($this->config["input"]){
-            default://csv
-                $incomeFile = "src/books.csv";
-                $reader = Reader::createFromPath($incomeFile);
+        switch ($this->config["input"]) {
+            case "csv":
+                $inputFile = $_SERVER["DOCUMENT_ROOT"] . $this->config["input_file_path"];
+                $reader = Reader::createFromPath($inputFile);
                 $class = new CSVDataProvider($reader);
+                break;
+            default:
+                throw new \Exception("Input format " . $this->config["input"] . " does not exist");
         }
 
         return $class;
     }
+
     public function getOutputHandler()
     {
-        switch ($this->config["output"]){
+        switch ($this->config["output"]) {
             case "file":
-                $outputFile = "dst/books.html";
+                $outputFile = $_SERVER["DOCUMENT_ROOT"] . $this->config["output_file_path"];
                 $class = new FileOutput($outputFile);
                 break;
-            default://csv
+            case "screen":
                 $class = new ScreenOutput();
+                break;
+            default:
+                throw new \Exception("Output type " . $this->config["output"] . " does not exist");
         }
 
         return $class;
     }
+
     public function getOutputFormatHandler()
     {
-        switch ($this->config["output_format"]){
+        switch ($this->config["output_format"]) {
             case "json":
-                $jsonRoot = "books";
+                $jsonRoot = $this->config["json_root"];
                 $class = new JsonConverter($jsonRoot);
                 break;
-            default://csv
-                $htmlView = "books";
+            case "html":
+                $htmlView = $this->config["html_view"];
                 $class = new HTMLConverter($htmlView);
+                break;
+            default:
+                throw new \Exception("Output format " . $this->config["output_format"] . " does not exist");
         }
 
         return $class;
